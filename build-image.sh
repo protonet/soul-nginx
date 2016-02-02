7a04d8d20048ae13e168316b221fef92a97f3fff
Christoph Olszowka <christoph@olszowka.de>
2016-02-02 15:26:52 +0100

    Fix an error with our nginx conf syntax, let nginx test our config before pushing a new image

--- build-image.sh
+++ build-image.sh
@@ -10,5 +10,13 @@ IMAGE_NAME=$HUB_REPOSITORY:$TRAVIS_BRANCH
 
 # Build the image
 docker build --pull -t $IMAGE_NAME .
+
+# Verify the config is ok
+docker run -i -t --rm \
+  --add-host soul-socketio:127.0.0.1 \
+  --add-host soul-web:127.0.0.1 \
+  --add-host soul-webdav:127.0.0.1 \
+  $IMAGE_NAME nginx -t
+
 # Push the image
 docker push $IMAGE_NAME
