938d19019bc1c19a4191bb738f9d920dab4b5049
Kamil Domański <kamil@domanski.co>
2016-04-08 10:38:00 +0200

    switch from Docker Hub to quay.io

--- build-image.sh
+++ build-image.sh
@@ -3,20 +3,20 @@
 set -e
 
 docker info
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e "none" -u "$QUAY_USER" -p "$QUAY_PASS" quay.io
 
-HUB_REPOSITORY="experimentalplatform/soul-nginx"
-IMAGE_NAME=$HUB_REPOSITORY:$TRAVIS_BRANCH
+HUB_REPOSITORY="quay.io/protonetinc/soul-nginx"
+IMAGE_NAME="$HUB_REPOSITORY:$TRAVIS_BRANCH"
 
 # Build the image
-docker build --pull -t $IMAGE_NAME .
+docker build --pull -t "$IMAGE_NAME" .
 
 # Verify the config is ok
 docker run -i -t --rm \
   --add-host soul-socketio:127.0.0.1 \
   --add-host soul-web:127.0.0.1 \
   --add-host soul-webdav:127.0.0.1 \
-  $IMAGE_NAME nginx -t
+  "$IMAGE_NAME" nginx -t
 
 # Push the image
-docker push $IMAGE_NAME
+docker push "$IMAGE_NAME"
